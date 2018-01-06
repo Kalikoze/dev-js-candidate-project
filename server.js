83bb0f9e73e33bf7db888d9dd52b559bfbc53074
Travis Rollins <kalikoze@gmail.com>
2018-01-06 10:46:06 -0700

    Fix eslint errors on server.js.

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare()
-.then(() => {
-  const server = express();
+  .then(() => {
+    const server = express();
 
-  server.get('*', (req, res) => handle(req, res))
+    server.get('*', (req, res) => handle(req, res));
 
-  server.listen(3000, err => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    server.listen(3000, err => {
+      if (err) throw err;
+      /* eslint-disable no-console */
+      console.log('> Ready on http://localhost:3000');
+    });
   })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+  .catch((ex) => {
+    /* eslint-disable no-console */
+    console.error(ex.stack);
+    process.exit(1);
+  });
