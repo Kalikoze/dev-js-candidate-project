555942e476a98a310e3ef1df54fa8f7db03ab7d6
Travis Rollins <kalikoze@gmail.com>
2018-01-08 17:23:30 -0700

    Create submit button.  Fix bug if Watson response doesn't have intent.

--- components/Chat.js
+++ components/Chat.js
@@ -14,7 +14,7 @@ class Chat extends Component {
   }
 
   async submit(e) {
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13 || e.button === 0) {
       const { input } = this.state;
       const { addMessage, postMessage } = this.props;
 
@@ -23,7 +23,7 @@ class Chat extends Component {
 
       const { message, intent } = this.props.response;
 
-      addMessage(message, intent)
+      addMessage(message, intent || '#dont_understand')
       this.setState({input: ''});
     }
   }
@@ -32,12 +32,13 @@ class Chat extends Component {
     const { input } = this.state;
     return (
       <section className='input-section'>
-        <input
-          placeholder='Chat with SpruceBot here...'
-          value={input}
-          onChange={e => this.setState({input: e.target.value})}
-          onKeyDown={e => this.submit(e)}
-        />
+          <input
+            placeholder='Chat with SpruceBot here...'
+            value={input}
+            onChange={e => this.setState({input: e.target.value})}
+            onKeyDown={e => this.submit(e)}
+          />
+          <button onClick={e => this.submit(e)}></button>
         <style jsx>{`
           .input-section {
             align-items: center;
@@ -48,7 +49,7 @@ class Chat extends Component {
 
           input {
             border: none;
-            border-radius: 20px;
+            border-radius: 25px;
             font-family: 'Lato', sans-serif;
             font-size: 2vw;
             height: 50px;
@@ -58,6 +59,21 @@ class Chat extends Component {
 
           input:focus {
             outline: none;
+          }
+
+          button {
+            background : url('../static/right-arrow.svg') #4B9DF6 no-repeat center / 50%;
+            border: none;
+            border-radius: 50%;
+            height: 40px;
+            outline: none;
+            position: relative;
+            right: 45px;
+            width: 40px;
+          }
+
+          button:active {
+            background-color: #8EBBF4;
           }
         `}</style>
       </section>
